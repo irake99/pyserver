230348e8fd284943fd001cf756761b2027a9d93e
Jack (Lung-Hsuan) Hung <lhhungx@gmail.com>
2022-06-03 19:28:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Basic FastAPI server for practice
+
+Getting started
+```shell
+# Prepare the environment
+python -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+
+# Run the server
+./main.py
+```
---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def helloworld_handler() -> str:
+    return 'Hello, world!'
+
+
+def main() -> None:
+    uvicorn.run('main:app', host="0.0.0.0", port=8080, reload=True)
+
+
+if __name__ == "__main__":
+    main()
