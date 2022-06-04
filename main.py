6c89728f79444f041fa6c6ea30c0334b8cba3608
Jack (Lung-Hsuan) Hung <lhhungx@gmail.com>
2022-06-04 17:33:15 +0800

    Add route page1 to render a HTML response

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ import uvicorn
 app = FastAPI()
 
 
+@app.get("/page1", response_class=HTMLResponse)
+async def page_handler(name: str = 'world') -> str:
+    my_response = f'''
+<html>
+    <h1>Hello, {name}!</h1>
+    <h2>It's heading level 2</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/4_Kittens.jpg/640px-4_Kittens.jpg" width="520">
+    <figcaption>License: <a href="https://commons.wikimedia.org/wiki/File:4_Kittens.jpg" target=_blank>Wikimedia Commons</a></figcaption>
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+    <a href="https://www.google.com" target=_blank>Link to Google</a>
+</html>'''
+
+    return my_response
+
+
 @app.get("/", response_class=HTMLResponse)
 async def helloworld_handler(name: str = 'world') -> str:
     return f'Hello, {name}!'
